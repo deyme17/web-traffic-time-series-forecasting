a30bb6b08f73c637c227cffed4f602e4e5710756
dmytro <dmytro19022006@gmail.com>
2026-06-05 14:37:20 +0300

    no need in Quantile loss

--- losses/register_losses.py
+++ losses/register_losses.py
@@ -1,11 +1,9 @@
 from utils import Registry
 from torch.nn import HuberLoss, L1Loss
-from .quantile_loss import QuantileLoss
 from .smape_loss import SMAPELoss
 
 LOSSES = Registry()
 
 LOSSES.register("Huber")(HuberLoss)
 LOSSES.register("MAE")(L1Loss)
-LOSSES.register("Quantile")(QuantileLoss)
 LOSSES.register("SMAPE")(SMAPELoss)
