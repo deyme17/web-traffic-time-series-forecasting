2f5150b9c9647865d43fe38ffcc01c3520cf2806
dmytro <dmytro19022006@gmail.com>
2026-06-08 12:51:03 +0300

    abc

--- losses/custom_loss.py
+++ losses/custom_loss.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
 import torch.nn as nn
 import torch
 
 
-class CustomLoss(nn.Module):
-    """Class-interface for custom loss function implementations."""
-    def forward(self, pred: torch.Tensor, target: torch.Tensor, 
+class CustomLoss(nn.Module, ABC):
+    """Base interface for custom loss functions."""
+    @abstractmethod
+    def forward(self,pred: torch.Tensor, target: torch.Tensor,
                 mask: torch.Tensor|None = None) -> torch.Tensor:
-        raise NotImplementedError("Subclasses must implement this method!")
+        pass
