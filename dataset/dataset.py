275529a2ac0327ee811f80720dac1a97c670d354
dmytro <dmytro19022006@gmail.com>
2026-06-09 18:20:07 +0300

    del some properties

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -90,14 +90,6 @@ class WTTSF_Dataset(Dataset):
     @property
     def dec_dim(self) -> int:
         return DEC_DIM
-    
-    @property
-    def lookback(self) -> int:
-        return self.lookback
-    
-    @property
-    def horizon(self) -> int:
-        return self.horizon
 
     def __len__(self) -> int:
         return self.n_pages
